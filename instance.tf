8518422c670d0345b58c559e81fd19cacef93785
Ubuntu <ubuntu@ip-172-31-37-118.ap-south-1.compute.internal>
2023-04-26 04:26:42 +0000

    one click code

--- instance.tf
+++ instance.tf
@@ -1,36 +1,31 @@
-resource "aws_key_pair" "my-key" {
-  key_name = "my-key"
+resource "aws_key_pair" "newkey" {
+  key_name = "newkey"
   public_key = "${file("${var.PUB_KEY_PATH}")}"
 }
 resource "aws_instance" "my_instance" {
     ami = "${lookup(var.AMIS, var.AWS_REGION)}"
     instance_type = "t2.micro"
     availability_zone = "ap-south-1a"
-    key_name = "${aws_key_pair.my-key.key_name}"
+    key_name = "${aws_key_pair.newkey.key_name}"
     tags = {
       name = "New_instance"
     }
-
-    provisioner "file" {
-      source = "new.sh"
-      destination = "/tmp/new.sh"
-    }
     provisioner "remote-exec" {
-      inline = [
-        "chmod +x /tmp/new.sh",
-        "sudo /tmp/new.sh"
-      ]
-    }
+      inline = ["echo 'Wait until SSH is ready'"]
+
     connection {
-      type = "ssh"
-      host = self.public_ip
-      user = "${var.USER}"
+      type        = "ssh"
+      user        = "${var.USER}"
       private_key = "${file("${var.PRIV_KEY_PATH}")}"
-      
+      host        = aws_instance.my_instance.public_ip
     }
+  }
+    provisioner "local-exec" {
+      command = "ansible-playbook  -i ${aws_instance.my_instance.public_ip}, --private-key ${var.PRIV_KEY_PATH} ubuntu.yaml"
+  }
 }
 
 output "instance_public_ip" {
-  value = "${aws_instance.my_instance.public_ip}"
+  value = aws_instance.my_instance.public_ip
 }
 
